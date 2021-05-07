5667ed456ec3c84e8bb42f0d7bcb66feee111177
Rodion Sibov <rodionsibov@gmail.com>
2021-05-07 17:27:44 +0300

    initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :id="id" class="board" @dragover.prevent @drop.prevent="drop">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  setup() {
+    function drop(e) {
+      const card_id = e.dataTransfer.getData("card_id");
+      const card = document.getElementById(card_id);
+      card.style.display = "block";
+      e.target.appendChild(card);
+    }
+
+    return {
+      drop,
+    };
+  },
+};
+</script>
